f34a3ae44415ee07ee9dc12490a35e6f9821020b
Oliver Chalk <oliverchalk245@gmail.com>
2022-07-13 16:24:32 -0500

    feat: implement and test config merging
    Currently, this feature does not work in tests because of the way
    that foundry setEnv works (it persists across tests and test
    files). This plus the non-determinism with when the env gets set
    and reset means these tests won't parse until there is an upstream
    change from foundry.

--- test/Solenv.t.sol
+++ test/Solenv.t.sol
@@ -4,54 +4,100 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import {Solenv} from "src/Solenv.sol";
 
-contract SolenvTest is Test {
-    function setUp() public {}
+library SolenvHelper {
+    address constant private VM_ADDRESS =
+        address(bytes20(uint160(uint256(keccak256('hevm cheat code')))));
+    Vm constant private vm = Vm(VM_ADDRESS);
 
+    function resetEnv() internal {
+        vm.setEnv("WHY_USE_THIS_KEY",               "");
+        vm.setEnv("SOME_VERY_IMPORTANT_API_KEY",    "");
+        vm.setEnv("A_COMPLEX_ENV_VARIABLE",         "");
+        vm.setEnv("A_NUMBER",                       "");
+        vm.setEnv("A_TRUE_BOOL",                    "");
+        vm.setEnv("A_FALSE_BOOL",                   "");
+        vm.setEnv("A_FALSE_BOOL",                   "");
+        vm.setEnv("AN_ADDRESS",                     "");
+        vm.setEnv("A_BYTES_32",                     "");
+    }
+}
+
+contract SolenvTest is Test {
     function testEnvLoad() public {
+        // act
         Solenv.config();
 
-        assertEq(vm.envString("WHY_USE_THIS_KEY"), "because we can can can");
-        assertEq(vm.envString("SOME_VERY_IMPORTANT_API_KEY"), "omgnoway");
-        assertEq(vm.envString("A_COMPLEX_ENV_VARIABLE"), "y&2U9xiEINv!vM8Gez");
-
-        assertEq(vm.envUint("A_NUMBER"), 100);
-
-        assertEq(vm.envBool("A_TRUE_BOOL"), true);
-        assertEq(vm.envBool("A_FALSE_BOOL"), false);
-
-        assertEq(vm.envBool("A_FALSE_BOOL"), false);
-
+        // assert
+        assertEq(vm.envString("WHY_USE_THIS_KEY"),              "because we can can can");
+        assertEq(vm.envString("SOME_VERY_IMPORTANT_API_KEY"),   "omgnoway");
+        assertEq(vm.envString("A_COMPLEX_ENV_VARIABLE"),        "y&2U9xiEINv!vM8Gez");
+        assertEq(vm.envUint("A_NUMBER"),                        100);
+        assertEq(vm.envBool("A_TRUE_BOOL"),                     true);
+        assertEq(vm.envBool("A_FALSE_BOOL"),                    false);
+        assertEq(vm.envBool("A_FALSE_BOOL"),                    false);
         assertEq(vm.envAddress("AN_ADDRESS"), 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-
         assertEq(vm.envBytes32("A_BYTES_32"), 0x0000000000000000000000000000000000000000000000000000000000000010);
+
+        // cleanup
+        SolenvHelper.resetEnv();
     }
 
     function testAnotherFileName() public {
         Solenv.config(".env.test");
 
         assertEq(vm.envString("SOME_VERY_IMPORTANT_API_KEY"), "adifferentone");
+
+        SolenvHelper.resetEnv();
+    }
+}
+
+contract SolenvMergeTest is Test {
+    function testEnvMerge() public {
+        // arrange
+        vm.setEnv("WHY_USE_THIS_KEY",   "different value");
+        vm.setEnv("A_NUMBER",           "1337");
+        vm.setEnv("A_TRUE_BOOL",        "false");
+        vm.setEnv("A_FALSE_BOOL",       "true");
+
+        // act
+        Solenv.config(false);
+
+        // assert
+        // from file
+        assertEq(vm.envString("SOME_VERY_IMPORTANT_API_KEY"),   "omgnoway");
+        assertEq(vm.envString("A_COMPLEX_ENV_VARIABLE"),        "y&2U9xiEINv!vM8Gez");
+        assertEq(vm.envAddress("AN_ADDRESS"), 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        assertEq(vm.envBytes32("A_BYTES_32"), 0x0000000000000000000000000000000000000000000000000000000000000010);
+
+        // set manually
+        assertEq(vm.envString("WHY_USE_THIS_KEY"),  "different value");
+        assertEq(vm.envUint("A_NUMBER"),            1337);
+        assertEq(vm.envBool("A_TRUE_BOOL"),         false);
+        assertEq(vm.envBool("A_FALSE_BOOL"),        true);
+
+        // cleanup
+        SolenvHelper.resetEnv();
     }
 }
 
 contract SolenvInSetupTest is Test {
     function setUp() public {
+        // act
         Solenv.config();
     }
 
     function testEnvLoad() public {
-        assertEq(vm.envString("WHY_USE_THIS_KEY"), "because we can can can");
-        assertEq(vm.envString("SOME_VERY_IMPORTANT_API_KEY"), "omgnoway");
-        assertEq(vm.envString("A_COMPLEX_ENV_VARIABLE"), "y&2U9xiEINv!vM8Gez");
-
-        assertEq(vm.envUint("A_NUMBER"), 100);
-
-        assertEq(vm.envBool("A_TRUE_BOOL"), true);
-        assertEq(vm.envBool("A_FALSE_BOOL"), false);
-
-        assertEq(vm.envBool("A_FALSE_BOOL"), false);
-
+        // assert
+        assertEq(vm.envString("WHY_USE_THIS_KEY"),              "because we can can can");
+        assertEq(vm.envString("SOME_VERY_IMPORTANT_API_KEY"),   "omgnoway");
+        assertEq(vm.envString("A_COMPLEX_ENV_VARIABLE"),        "y&2U9xiEINv!vM8Gez");
+        assertEq(vm.envUint("A_NUMBER"),                        100);
+        assertEq(vm.envBool("A_TRUE_BOOL"),                     true);
+        assertEq(vm.envBool("A_FALSE_BOOL"),                    false);
+        assertEq(vm.envBool("A_FALSE_BOOL"),                    false);
         assertEq(vm.envAddress("AN_ADDRESS"), 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-
         assertEq(vm.envBytes32("A_BYTES_32"), 0x0000000000000000000000000000000000000000000000000000000000000010);
+
+        SolenvHelper.resetEnv();
     }
 }
